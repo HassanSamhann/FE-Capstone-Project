e5a169dc7c89c019317a6d9f5a94d5a47a1c2593
hassan <hassan700019@gmail.com>
2024-09-28 14:56:26 +0300

    good

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,8 +9,10 @@ const Header = () => {
         <header className='header'>
             <Navbar />
             <div className='header-content flex flex-c text-center text-white'>
-                <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
-                <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
+                <h2 className='header-title text-capitalize'>Welcome to Book_Bridge</h2><br />
+                <p className='header-text fs-18 fw-3'>Discover Your Next Great Read – Explore, Search, and Enjoy a World of Books! Immerse yourself in a vast collection of literary treasures. Whether you're seeking fiction, non-fiction, or hidden gems, we have something for every book lover. Join our community of readers and embark on your next reading adventure today!
+
+</p>
                 <SearchForm />
             </div>
         </header>
