cc864940b8193bcd3c1903facb661af524c155e4
hassan <hassan700019@gmail.com>
2024-09-30 00:48:12 +0300

    speed test vertical

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Home from './pages/Home/Home';
 import About from "./pages/About/About";
 import BookList from "./components/BookList/BookList";
 import BookDetails from "./components/BookDetails/BookDetails";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,6 +22,8 @@ root.render(
           <Route path = "/book/:id" element = {<BookDetails />} />
         </Route>
       </Routes>
+      <SpeedInsights />
+
     </BrowserRouter>
   </AppProvider>
 );
